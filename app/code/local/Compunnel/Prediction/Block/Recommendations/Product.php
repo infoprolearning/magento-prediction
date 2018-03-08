b99bf43c3a72c8a498d32f2acbe9ff997885b9f8
Prateek Agrawal <prateek.agarwal@compunnel.com>
2018-03-08 12:32:22 +0530

    block level preparations for product page

--- app/code/local/Compunnel/Prediction/Block/Recommendations/Product.php
+++ app/code/local/Compunnel/Prediction/Block/Recommendations/Product.php
@@ -1,3 +1,9 @@
 <?php
     class Compunnel_Prediction_Block_Recommendations_Product extends Compunnel_Prediction_Block_Recommendations_Abstract {
+
+        protected function _construct() {
+            parent::_construct();
+            $this->setTitle($this->__('Titles you might be interested in'));
+        }
+
     }
