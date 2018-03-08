665f51055c6a989da99d60ee14a077dd968d41fc
Prateek Agrawal <prateek.agarwal@compunnel.com>
2018-03-08 12:26:46 +0530

    Completed hard-coded predictions for home page

--- app/code/local/Compunnel/Prediction/Block/Recommendations/Home.php
+++ app/code/local/Compunnel/Prediction/Block/Recommendations/Home.php
@@ -1,3 +1,9 @@
 <?php
     class Compunnel_Prediction_Block_Recommendations_Home extends Compunnel_Prediction_Block_Recommendations_Abstract {
+
+        protected function _construct() {
+            parent::_construct();
+            $this->setTitle($this->__('Titles you might be interested in'));
+        }
+
     }
