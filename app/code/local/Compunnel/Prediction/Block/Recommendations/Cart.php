f15266c4cb52933c9a0f436e344a6df6ae1d840f
Prateek Agrawal <prateek.agarwal@compunnel.com>
2018-03-19 18:18:36 +0530

    Dynamic behavior in product and cart pages

--- app/code/local/Compunnel/Prediction/Block/Recommendations/Cart.php
+++ app/code/local/Compunnel/Prediction/Block/Recommendations/Cart.php
@@ -6,4 +6,54 @@
             $this->setTitle($this->__('Titles you might be interested in'));
         }
 
+        protected function _getProductCollection() {
+            if (is_null($this->_productCollection)) {
+
+                $data = array();
+                $data['num'] = 5;
+                $data['itemSet'] = $this->_getCartProductIds();
+
+                $result = Mage::helper('prediction')->makeRecommendationCall($data);
+
+                $results = json_decode($result, true);
+                if (isset($results['itemScores']) && !empty($results['itemScores'])) {
+                    $apiProducts = array();
+                    foreach ($results['itemScores'] as $key => $value) {
+                        $apiProducts[] = $value['item'];
+                    }
+
+                    $collection = Mage::getModel('catalog/product')->getCollection()
+                        ->addAttributeToFilter('entity_id', array('in' => $apiProducts));
+
+                    $collection->setVisibility(Mage::getSingleton('catalog/product_visibility')->getVisibleInCatalogIds());
+
+                    $collection = $this->_addProductAttributesAndPrices($collection)
+                        ->addStoreFilter()
+                        ->setPageSize($this->getProductsCount())
+                        ->setCurPage(1);
+                    $this->_productCollection = $collection;
+                }
+            }
+
+            return $this->_productCollection;
+        }
+
+        protected function _getCartProductIds() {
+            $ids = $this->getData('_cart_product_ids');
+            if (is_null($ids)) {
+                $ids = array();
+                foreach ($this->getQuote()->getAllItems() as $item) {
+                    if ($product = $item->getProduct()) {
+                        $ids[] = $product->getId();
+                    }
+                }
+                $this->setData('_cart_product_ids', $ids);
+            }
+            return $ids;
+        }
+
+        public function getQuote() {
+            return Mage::getSingleton('checkout/session')->getQuote();
+        }
+
     }
--- app/code/local/Compunnel/Prediction/Block/Recommendations/Product.php
+++ app/code/local/Compunnel/Prediction/Block/Recommendations/Product.php
@@ -6,4 +6,45 @@
             $this->setTitle($this->__('Titles you might be interested in'));
         }
 
+        protected function _getProductCollection() {
+            if (is_null($this->_productCollection)) {
+
+                $data = array();
+                if (Mage::getSingleton('customer/session')->isLoggedIn()) {
+                    $customerData = Mage::getSingleton('customer/session')->getCustomer();
+                    $data['user'] = $customerData->getId();
+                }
+
+                $product = Mage::registry('product');
+
+                $itemId = $product->getId();
+                $data['item'] = $itemId;
+                $data['num'] = 5;
+                $data['returnSelf'] = false;
+
+                $result = Mage::helper('prediction')->makeRecommendationCall($data);
+
+                $results = json_decode($result, true);
+                if (isset($results['itemScores']) && !empty($results['itemScores'])) {
+                    $apiProducts = array();
+                    foreach ($results['itemScores'] as $key => $value) {
+                        $apiProducts[] = $value['item'];
+                    }
+
+                    $collection = Mage::getModel('catalog/product')->getCollection()
+                        ->addAttributeToFilter('entity_id', array('in' => $apiProducts));
+
+                    $collection->setVisibility(Mage::getSingleton('catalog/product_visibility')->getVisibleInCatalogIds());
+
+                    $collection = $this->_addProductAttributesAndPrices($collection)
+                        ->addStoreFilter()
+                        ->setPageSize($this->getProductsCount())
+                        ->setCurPage(1);
+                    $this->_productCollection = $collection;
+                }
+            }
+
+            return $this->_productCollection;
+        }
+
     }
